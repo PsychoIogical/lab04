ea89c6ee8f881aac8858886b97999bcdc55dd9a5
Dalton Yoder <dyoder97@gmail.com>
2019-10-08 00:25:03 -0500

    Finished Exercise 3.

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -1,4 +1,5 @@
 <?php
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
 /* Variables */
 $name = $_POST["user"];
 $password = $_POST["password"];
@@ -10,14 +11,74 @@ $yellow = $_POST["yellow"];
 /* Shipping */
 $shipping = $_POST["shipping"];
 
-echo "Welcome " . $name . " to the Block Plaza!<br>";
-echo "You do know that " . $password . " isn't a good password, right?<br>";
+/* Math */
+$blueCost = 2;
+$redCost = 3;
+$greenCost = 2;
+$yellowCost = 1;
+/* Subtotals */
+$blueSubTotal = $blue * $blueCost;
+$redSubTotal = $red * $redCost;
+$greenSubTotal = $green * $greenCost;
+$yellowSubTotal = $yellow * $yellowCost;
+/* Shipping */
+$shippingCost = 0;
+if($shipping == "overnight") {
+  $shipping = "Overnight";
+  $shippingCost = 40;
+} elseif ($shipping == "two") {
+  $shipping = "Two Days";
+  $shippingCost = 20;
+} else {
+  $shipping = "Free Shipping";
+  $shippingCost = 0;
+}
+/* Total */
+$totalCost = $blueSubTotal + $redSubTotal + $greenSubTotal + $yellowSubTotal + $shippingCost;
 
-echo "You want " . $blue . " blue boxes.<br>";
-echo "You want " . $red . " red boxes.<br>";
-echo "You want " . $green . " green boxes.<br>";
-echo "You want " . $yellow . " yellow boxes.<br>";
+echo "<h2>Welcome " . $name . " to the Block Plaza!</h2><br>";
+echo "You do know that " . $password . " isn't a good password, right?<br><br>";
 
-echo "All with " . $shipping . " shipping.<br>";
+echo "Here is your receipt!<br><br>";
 
+echo "<table><tr>
+      <td> </td>
+      <td class=\"desc\"><b>Quantity</b></td>
+      <td class=\"desc\"><b>Cost Per Item</b></td>
+      <td class=\"desc\"><b>Sub Total</b></td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\"><b>Blue</b></td>
+      <td>" . $blue . "</td>
+      <td>" . $blueCost . "</td>
+      <td>" . $blueSubTotal . "</td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\"><b>Red</b></td>
+      <td>" . $red . "</td>
+      <td>" . $redCost . "</td>
+      <td>" . $redSubTotal . "</td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\"><b>Green</b></td>
+      <td>" . $green . "</td>
+      <td>" . $greenCost . "</td>
+      <td>" . $greenSubTotal . "</td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\"><b>Yellow</b></td>
+      <td>" . $yellow . "</td>
+      <td>" . $yellowCost . "</td>
+      <td>" . $yellowSubTotal . "</td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\"><b>Shipping</b></td>
+      <td colspan=\"2\">" . $shipping . "</td>
+      <td>" . $shippingCost . "</td>
+      </tr>";
+echo "<tr>
+      <td class=\"desc\" colspan=\"3\"><b>Total Cost</b></td>
+      <td class=\"desc\">" . $totalCost . "</td>
+      </tr></table>";
+echo "<br><h3>Thank you for your purchase, have a great day!</h3><br>";
 ?>
